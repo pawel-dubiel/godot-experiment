379303b1b805995d657c970e3ac085228e861ca3
Pawel Dubiel <contact@paweldubiel.com>
2026-01-18 00:02:27 +0100

    feat: Implement hex distance calculation in MapService

--- scripts/systems/MapService.gd
+++ scripts/systems/MapService.gd
@@ -21,3 +21,18 @@ func initialize_map(new_tiles: Dictionary) -> void:
 	for coord in new_tiles:
 		model.set_tile(coord, new_tiles[coord])
 	map_updated.emit()
+
+## Calculates distance between two hex coordinates (Cube/Axial distance).
+func get_distance(a: Vector2i, b: Vector2i) -> int:
+	# Using Axial coordinates (q, r) conversion to Cube (x, y, z)
+	# q = x, r = y
+	# s = -q - r
+	var aq = a.x
+	var ar = a.y
+	var as_ = -aq - ar
+	
+	var bq = b.x
+	var br = b.y
+	var bs = -bq - br
+	
+	return int((abs(aq - bq) + abs(ar - br) + abs(as_ - bs)) / 2)
